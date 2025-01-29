d17626e57c847542b9a1744caf2ef4321d09265e
annavighi <vighianna@gmail.com>
2025-01-29 13:08:30 -0300

    design alterations made

--- src/pages/components/navbar/Navbar.tsx
+++ src/pages/components/navbar/Navbar.tsx
@@ -2,15 +2,15 @@ function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
+            			   bg-orange-700 text-orange-50'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                    Home
 
                     <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
+                        Restaurantes
+                        Seleção fit
+                        Categorias
                         Perfil
                         Sair
                     </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,28 +2,28 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
+            <div className="bg-orange-50 flex justify-center">
+                <div className='container grid grid-cols-2 text-orange-700'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            Fast Feast
                         </h2>
                         <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
+                            Chega rápido e come bem!
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
+                            <div className='rounded text-orange-50
+                                            text-orange-50 border-orange-700 bg-orange-700 border-solid border-2 py-2 px-4 font-semibold'
                                 >
-                                Nova Postagem
+                                Peça já!
                             </div>
                         </div>
                     </div>
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src='home.png'
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
